979ba4ccb21bb9e45d7bbba7f431b03524578890
klamt1ne <klamtine@gmail.com>
2014-09-15 15:49:20 +0800

    add module-system support

--- validator.coffee
+++ validator.coffee
@@ -5,162 +5,182 @@ Licensed under MIT
 
 'use strict'
 
+do (factory) ->
+  if typeof define is "function" and define.amd
+    
+    # AMD
+    define ["jquery"], factory
+  else if typeof exports is "object"
+    
+    # CommonJS
+    factory require("jquery")
+  else if typeof define is "function" and define.cmd
+    
+    # CMD
+    define (require) ->
+      require "jquery"
+      factory $
+      return
 
+  else
+    window.Validator = factory(jQuery)
+  return
 
-class Validator
-	constructor: (element) ->
-		@$element = $(element)
+factory = () ->
+	class Validator
+		constructor: (element) ->
+			@$element = $(element)
 
-trim = () ->
-	return @.replace(/^\s+|\s+$/g, '')
+	trim = () ->
+		return @.replace(/^\s+|\s+$/g, '')
 
-# Validator验证通过后的数据
-Validator::result = {}
+	# Validator验证通过后的数据
+	Validator::result = {}
 
-# Validator预设值
-Validator::settings = 
-	MAXLENGTH: {}
-	MINLENGTH: {}
-	ATMOST: {}
-	ATLEAST: {}
-	REPEAT: {}
+	# Validator预设值
+	Validator::settings = 
+		MAXLENGTH: {}
+		MINLENGTH: {}
+		ATMOST: {}
+		ATLEAST: {}
+		REPEAT: {}
 
-Validator::validate = (config, callback) ->
-	MAXLENGTH_PATTERN = /maxLength=/i
-	MINLENGTH_PATTERN = /minLength=/i
-	ATMOST_PATTERN    = /atMost=/i
-	ATLEAST_PATTERN   = /atLeast=/i
-	
-	PATTERNS = [
-		MAXLENGTH_PATTERN
-		MINLENGTH_PATTERN
-		ATMOST_PATTERN
-		ATLEAST_PATTERN
-	]
+	Validator::validate = (config, callback) ->
+		MAXLENGTH_PATTERN = /maxLength=/i
+		MINLENGTH_PATTERN = /minLength=/i
+		ATMOST_PATTERN    = /atMost=/i
+		ATLEAST_PATTERN   = /atLeast=/i
+		
+		PATTERNS = [
+			MAXLENGTH_PATTERN
+			MINLENGTH_PATTERN
+			ATMOST_PATTERN
+			ATLEAST_PATTERN
+		]
 
-	@isOnParent = config.isOnParent ? true
-	@errorClass = config.errorClass || 'has-error'
+		@isOnParent = config.isOnParent ? true
+		@errorClass = config.errorClass || 'has-error'
 
-	for key of config.msg when config.msg.hasOwnProperty(key)
-		types = key
-		identifier = "*[data-validator=#{key}]"
-		$input = @$element.find($(identifier))
-		rules = $input.data('rules').split(',')
+		for key of config.msg when config.msg.hasOwnProperty(key)
+			types = key
+			identifier = "*[data-validator=#{key}]"
+			$input = @$element.find($(identifier))
+			rules = $input.data('rules').split(',')
 
-		for pattern in PATTERNS
-			for rule, i in rules
-				if (rule.trim().match(pattern))
-					index = rule.indexOf('=')
-					switch pattern
-						when MAXLENGTH_PATTERN
-							@settings.MAXLENGTH[key] = parseInt rule.substring(index + 1), 10
-							rules[i] = 'maxLength'
-						when MINLENGTH_PATTERN
-							@settings.MINLENGTH[key] = parseInt rule.substring(index + 1), 10
-							rules[i] = 'minLength'
-						when ATMOST_PATTERN
-							@settings.ATMOST[key] = parseInt rule.substring(index + 1), 10
-							rules[i] = 'atMost'
-						when ATLEAST_PATTERN
-							@settings.ATLEAST[key] = parseInt rule.substring(index + 1), 10
-							rules[i] = 'atLeast'
+			for pattern in PATTERNS
+				for rule, i in rules
+					if (rule.trim().match(pattern))
+						index = rule.indexOf('=')
+						switch pattern
+							when MAXLENGTH_PATTERN
+								@settings.MAXLENGTH[key] = parseInt rule.substring(index + 1), 10
+								rules[i] = 'maxLength'
+							when MINLENGTH_PATTERN
+								@settings.MINLENGTH[key] = parseInt rule.substring(index + 1), 10
+								rules[i] = 'minLength'
+							when ATMOST_PATTERN
+								@settings.ATMOST[key] = parseInt rule.substring(index + 1), 10
+								rules[i] = 'atMost'
+							when ATLEAST_PATTERN
+								@settings.ATLEAST[key] = parseInt rule.substring(index + 1), 10
+								rules[i] = 'atLeast'
 
-		for rule in rules
-			result = @[rule.trim()](key, $input.val()?.trim())
-			$input.parent().removeClass(@errorClass)
+			for rule in rules
+				result = @[rule.trim()](key, $input.val()?.trim())
+				$input.parent().removeClass(@errorClass)
 
-			unless result.pass
-				if @isOnParent then $input.focus().parent().addClass(@errorClass) else $input.focus().addClass(@errorClass)
-				return callback({pass: false, msg: config.msg[key][rule.trim()]})
+				unless result.pass
+					if @isOnParent then $input.focus().parent().addClass(@errorClass) else $input.focus().addClass(@errorClass)
+					return callback({pass: false, msg: config.msg[key][rule.trim()]})
 
-	callback({pass: true}, @result)
+		callback({pass: true}, @result)
 
-Validator::notEmpty = (name, value) ->
-	regex = /^\s+$/
-	result = value.length && !regex.test(value)
+	Validator::notEmpty = (name, value) ->
+		regex = /^\s+$/
+		result = value.length && !regex.test(value)
 
-	if result
-		@result[name] = value
-		return {pass: true}
+		if result
+			@result[name] = value
+			return {pass: true}
 
-	{pass: false}
+		{pass: false}
 
-Validator::email = (name, value) ->
-	regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-	result = regex.test(value)
+	Validator::email = (name, value) ->
+		regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		result = regex.test(value)
 
-	if result
-		@result[name] = value
-		return {pass: true}
+		if result
+			@result[name] = value
+			return {pass: true}
 
-	{pass: false}
+		{pass: false}
 
-Validator::phone = (name, value) ->
-	regex = /^1[3-9]\d{9}$/
-	result = regex.test(value)
+	Validator::phone = (name, value) ->
+		regex = /^1[3-9]\d{9}$/
+		result = regex.test(value)
 
-	if result
-		@result[name] = value
-		return {pass: true}
+		if result
+			@result[name] = value
+			return {pass: true}
 
-	{pass: false}
+		{pass: false}
 
-Validator::minLength = (name, value) ->
-    len = value.length
-    minLen = parseInt(@settings.MINLENGTH[name], 10)
+	Validator::minLength = (name, value) ->
+	    len = value.length
+	    minLen = parseInt(@settings.MINLENGTH[name], 10)
 
-    if len >= minLen
-        @result[name] = value
-        return {pass: true}
+	    if len >= minLen
+	        @result[name] = value
+	        return {pass: true}
 
-    {pass: false}
+	    {pass: false}
 
-Validator::maxLength = (name, value) ->
-    len = value.length
-    maxLen = parseInt(@settings.MAXLENGTH[name], 10)
+	Validator::maxLength = (name, value) ->
+	    len = value.length
+	    maxLen = parseInt(@settings.MAXLENGTH[name], 10)
 
-    if len <= maxLen
-        @result[name] = value
-        return {pass: true}
+	    if len <= maxLen
+	        @result[name] = value
+	        return {pass: true}
 
-    {pass: false}
+	    {pass: false}
 
-Validator::atLeast = (name, value) ->
-    len = $("*[data-validator=#{name}]:checked").length || $("*[data-validator=#{name}]:selected").length
-    minLen = @settings.ATLEAST[name]
+	Validator::atLeast = (name, value) ->
+	    len = $("*[data-validator=#{name}]:checked").length || $("*[data-validator=#{name}]:selected").length
+	    minLen = @settings.ATLEAST[name]
 
-    if len >= minLen
-        @result[name] = value
-        return {pass: true}
+	    if len >= minLen
+	        @result[name] = value
+	        return {pass: true}
 
-    {pass: false}
+	    {pass: false}
 
-Validator::atMost = (name, value) ->
-    len = $("*[data-validator=#{name}]:checked").length || $("*[data-validator=#{name}]:selected")
-    maxLen = @settings.ATMOST[name]
+	Validator::atMost = (name, value) ->
+	    len = $("*[data-validator=#{name}]:checked").length || $("*[data-validator=#{name}]:selected")
+	    maxLen = @settings.ATMOST[name]
 
-    if len <= maxLen
-        @result[name] = value
-        return {pass: true}
+	    if len <= maxLen
+	        @result[name] = value
+	        return {pass: true}
 
-    {pass: false}
+	    {pass: false}
 
-Validator::int = (name, value) ->
-    result = not isNaN(value)
+	Validator::int = (name, value) ->
+	    result = not isNaN(value)
 
-    if result
-        @result[name] = value
-        return {pass: true}
+	    if result
+	        @result[name] = value
+	        return {pass: true}
 
-    {pass: false}
+	    {pass: false}
 
-old = $.fn.validate
+	old = $.fn.validate
 
-$.fn.validate = (config, callback) ->
-	new Validator(@).validate(config, callback)
+	$.fn.validate = (config, callback) ->
+		new Validator(@).validate(config, callback)
 
-$.fn.validate.noConflict = ->
-	$.fn.validate = old
-	@
+	$.fn.validate.noConflict = ->
+		$.fn.validate = old
+		@
 
 
--- validator.js
+++ validator.js
@@ -7,224 +7,227 @@ Licensed under MIT
 
 (function() {
   'use strict';
-  var Validator, old, trim;
+  var factory;
 
-  Validator = (function() {
-    function Validator(element) {
-      this.$element = $(element);
+  (function(factory) {
+    if (typeof define === "function" && define.amd) {
+      define(["jquery"], factory);
+    } else if (typeof exports === "object") {
+      factory(require("jquery"));
+    } else if (typeof define === "function" && define.cmd) {
+      define(function(require) {
+        require("jquery");
+        factory($);
+      });
+    } else {
+      window.Validator = factory(jQuery);
     }
+  })(factory);
 
-    return Validator;
-
-  })();
-
-  trim = function() {
-    return this.replace(/^\s+|\s+$/g, '');
-  };
-
-  Validator.prototype.result = {};
-
-  Validator.prototype.settings = {
-    MAXLENGTH: {},
-    MINLENGTH: {},
-    ATMOST: {},
-    ATLEAST: {},
-    REPEAT: {}
-  };
-
-  Validator.prototype.validate = function(config, callback) {
-    var $input, ATLEAST_PATTERN, ATMOST_PATTERN, MAXLENGTH_PATTERN, MINLENGTH_PATTERN, PATTERNS, i, identifier, index, key, pattern, result, rule, rules, types, _i, _j, _k, _len, _len1, _len2, _ref, _ref1;
-    MAXLENGTH_PATTERN = /maxLength=/i;
-    MINLENGTH_PATTERN = /minLength=/i;
-    ATMOST_PATTERN = /atMost=/i;
-    ATLEAST_PATTERN = /atLeast=/i;
-    PATTERNS = [MAXLENGTH_PATTERN, MINLENGTH_PATTERN, ATMOST_PATTERN, ATLEAST_PATTERN];
-    this.onParent = (_ref = config.onParent) != null ? _ref : true;
-    this.errorClass = config.errorClass || 'has-error';
-    for (key in config.msg) {
-      if (!(config.msg.hasOwnProperty(key))) {
-        continue;
+  factory = function() {
+    var Validator, old, trim;
+    Validator = (function() {
+      function Validator(element) {
+        this.$element = $(element);
       }
-      types = key;
-      identifier = "*[data-validator=" + key + "]";
-      $input = this.$element.find($(identifier));
-      rules = $input.data('rules').split(',');
-      for (_i = 0, _len = PATTERNS.length; _i < _len; _i++) {
-        pattern = PATTERNS[_i];
-        for (i = _j = 0, _len1 = rules.length; _j < _len1; i = ++_j) {
-          rule = rules[i];
-          if (rule.trim().match(pattern)) {
-            index = rule.indexOf('=');
-            switch (pattern) {
-              case MAXLENGTH_PATTERN:
-                this.settings.MAXLENGTH[key] = parseInt(rule.substring(index + 1), 10);
-                rules[i] = 'maxLength';
-                break;
-              case MINLENGTH_PATTERN:
-                this.settings.MINLENGTH[key] = parseInt(rule.substring(index + 1), 10);
-                rules[i] = 'minLength';
-                break;
-              case ATMOST_PATTERN:
-                this.settings.ATMOST[key] = parseInt(rule.substring(index + 1), 10);
-                rules[i] = 'atMost';
-                break;
-              case ATLEAST_PATTERN:
-                this.settings.ATLEAST[key] = parseInt(rule.substring(index + 1), 10);
-                rules[i] = 'atLeast';
+
+      return Validator;
+
+    })();
+    trim = function() {
+      return this.replace(/^\s+|\s+$/g, '');
+    };
+    Validator.prototype.result = {};
+    Validator.prototype.settings = {
+      MAXLENGTH: {},
+      MINLENGTH: {},
+      ATMOST: {},
+      ATLEAST: {},
+      REPEAT: {}
+    };
+    Validator.prototype.validate = function(config, callback) {
+      var $input, ATLEAST_PATTERN, ATMOST_PATTERN, MAXLENGTH_PATTERN, MINLENGTH_PATTERN, PATTERNS, i, identifier, index, key, pattern, result, rule, rules, types, _i, _j, _k, _len, _len1, _len2, _ref, _ref1;
+      MAXLENGTH_PATTERN = /maxLength=/i;
+      MINLENGTH_PATTERN = /minLength=/i;
+      ATMOST_PATTERN = /atMost=/i;
+      ATLEAST_PATTERN = /atLeast=/i;
+      PATTERNS = [MAXLENGTH_PATTERN, MINLENGTH_PATTERN, ATMOST_PATTERN, ATLEAST_PATTERN];
+      this.isOnParent = (_ref = config.isOnParent) != null ? _ref : true;
+      this.errorClass = config.errorClass || 'has-error';
+      for (key in config.msg) {
+        if (!(config.msg.hasOwnProperty(key))) {
+          continue;
+        }
+        types = key;
+        identifier = "*[data-validator=" + key + "]";
+        $input = this.$element.find($(identifier));
+        rules = $input.data('rules').split(',');
+        for (_i = 0, _len = PATTERNS.length; _i < _len; _i++) {
+          pattern = PATTERNS[_i];
+          for (i = _j = 0, _len1 = rules.length; _j < _len1; i = ++_j) {
+            rule = rules[i];
+            if (rule.trim().match(pattern)) {
+              index = rule.indexOf('=');
+              switch (pattern) {
+                case MAXLENGTH_PATTERN:
+                  this.settings.MAXLENGTH[key] = parseInt(rule.substring(index + 1), 10);
+                  rules[i] = 'maxLength';
+                  break;
+                case MINLENGTH_PATTERN:
+                  this.settings.MINLENGTH[key] = parseInt(rule.substring(index + 1), 10);
+                  rules[i] = 'minLength';
+                  break;
+                case ATMOST_PATTERN:
+                  this.settings.ATMOST[key] = parseInt(rule.substring(index + 1), 10);
+                  rules[i] = 'atMost';
+                  break;
+                case ATLEAST_PATTERN:
+                  this.settings.ATLEAST[key] = parseInt(rule.substring(index + 1), 10);
+                  rules[i] = 'atLeast';
+              }
             }
           }
         }
-      }
-      for (_k = 0, _len2 = rules.length; _k < _len2; _k++) {
-        rule = rules[_k];
-        result = this[rule.trim()](key, (_ref1 = $input.val()) != null ? _ref1.trim() : void 0);
-        $input.parent().removeClass(this.errorClass);
-        if (!result.pass) {
-          if (this.onParent) {
-            $input.focus().parent().addClass(this.errorClass);
-          } else {
-            $input.focus().addClass(this.errorClass);
+        for (_k = 0, _len2 = rules.length; _k < _len2; _k++) {
+          rule = rules[_k];
+          result = this[rule.trim()](key, (_ref1 = $input.val()) != null ? _ref1.trim() : void 0);
+          $input.parent().removeClass(this.errorClass);
+          if (!result.pass) {
+            if (this.isOnParent) {
+              $input.focus().parent().addClass(this.errorClass);
+            } else {
+              $input.focus().addClass(this.errorClass);
+            }
+            return callback({
+              pass: false,
+              msg: config.msg[key][rule.trim()]
+            });
           }
-          return callback({
-            pass: false,
-            msg: config.msg[key][rule.trim()]
-          });
         }
       }
-    }
-    return callback({
-      pass: true
-    }, this.result);
-  };
-
-  Validator.prototype.notEmpty = function(name, value) {
-    var regex, result;
-    regex = /^\s+$/;
-    result = value.length && !regex.test(value);
-    if (result) {
-      this.result[name] = value;
-      return {
+      return callback({
         pass: true
-      };
-    }
-    return {
-      pass: false
+      }, this.result);
     };
-  };
-
-  Validator.prototype.email = function(name, value) {
-    var regex, result;
-    regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    result = regex.test(value);
-    if (result) {
-      this.result[name] = value;
+    Validator.prototype.notEmpty = function(name, value) {
+      var regex, result;
+      regex = /^\s+$/;
+      result = value.length && !regex.test(value);
+      if (result) {
+        this.result[name] = value;
+        return {
+          pass: true
+        };
+      }
       return {
-        pass: true
+        pass: false
       };
-    }
-    return {
-      pass: false
     };
-  };
-
-  Validator.prototype.phone = function(name, value) {
-    var regex, result;
-    regex = /^1[3-9]\d{9}$/;
-    result = regex.test(value);
-    if (result) {
-      this.result[name] = value;
+    Validator.prototype.email = function(name, value) {
+      var regex, result;
+      regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      result = regex.test(value);
+      if (result) {
+        this.result[name] = value;
+        return {
+          pass: true
+        };
+      }
       return {
-        pass: true
+        pass: false
       };
-    }
-    return {
-      pass: false
     };
-  };
-
-  Validator.prototype.minLength = function(name, value) {
-    var len, minLen;
-    len = value.length;
-    minLen = parseInt(this.settings.MINLENGTH[name], 10);
-    if (len >= minLen) {
-      this.result[name] = value;
+    Validator.prototype.phone = function(name, value) {
+      var regex, result;
+      regex = /^1[3-9]\d{9}$/;
+      result = regex.test(value);
+      if (result) {
+        this.result[name] = value;
+        return {
+          pass: true
+        };
+      }
       return {
-        pass: true
+        pass: false
       };
-    }
-    return {
-      pass: false
     };
-  };
-
-  Validator.prototype.maxLength = function(name, value) {
-    var len, maxLen;
-    len = value.length;
-    maxLen = parseInt(this.settings.MAXLENGTH[name], 10);
-    if (len <= maxLen) {
-      this.result[name] = value;
+    Validator.prototype.minLength = function(name, value) {
+      var len, minLen;
+      len = value.length;
+      minLen = parseInt(this.settings.MINLENGTH[name], 10);
+      if (len >= minLen) {
+        this.result[name] = value;
+        return {
+          pass: true
+        };
+      }
       return {
-        pass: true
+        pass: false
       };
-    }
-    return {
-      pass: false
     };
-  };
-
-  Validator.prototype.atLeast = function(name, value) {
-    var len, minLen;
-    len = $("*[data-validator=" + name + "]:checked").length || $("*[data-validator=" + name + "]:selected").length;
-    minLen = this.settings.ATLEAST[name];
-    if (len >= minLen) {
-      this.result[name] = value;
+    Validator.prototype.maxLength = function(name, value) {
+      var len, maxLen;
+      len = value.length;
+      maxLen = parseInt(this.settings.MAXLENGTH[name], 10);
+      if (len <= maxLen) {
+        this.result[name] = value;
+        return {
+          pass: true
+        };
+      }
       return {
-        pass: true
+        pass: false
       };
-    }
-    return {
-      pass: false
     };
-  };
-
-  Validator.prototype.atMost = function(name, value) {
-    var len, maxLen;
-    len = $("*[data-validator=" + name + "]:checked").length || $("*[data-validator=" + name + "]:selected");
-    maxLen = this.settings.ATMOST[name];
-    if (len <= maxLen) {
-      this.result[name] = value;
+    Validator.prototype.atLeast = function(name, value) {
+      var len, minLen;
+      len = $("*[data-validator=" + name + "]:checked").length || $("*[data-validator=" + name + "]:selected").length;
+      minLen = this.settings.ATLEAST[name];
+      if (len >= minLen) {
+        this.result[name] = value;
+        return {
+          pass: true
+        };
+      }
       return {
-        pass: true
+        pass: false
       };
-    }
-    return {
-      pass: false
     };
-  };
-
-  Validator.prototype.int = function(name, value) {
-    var result;
-    result = !isNaN(value);
-    if (result) {
-      this.result[name] = value;
+    Validator.prototype.atMost = function(name, value) {
+      var len, maxLen;
+      len = $("*[data-validator=" + name + "]:checked").length || $("*[data-validator=" + name + "]:selected");
+      maxLen = this.settings.ATMOST[name];
+      if (len <= maxLen) {
+        this.result[name] = value;
+        return {
+          pass: true
+        };
+      }
       return {
-        pass: true
+        pass: false
       };
-    }
-    return {
-      pass: false
     };
-  };
-
-  old = $.fn.validate;
-
-  $.fn.validate = function(config, callback) {
-    return new Validator(this).validate(config, callback);
-  };
-
-  $.fn.validate.noConflict = function() {
-    $.fn.validate = old;
-    return this;
+    Validator.prototype.int = function(name, value) {
+      var result;
+      result = !isNaN(value);
+      if (result) {
+        this.result[name] = value;
+        return {
+          pass: true
+        };
+      }
+      return {
+        pass: false
+      };
+    };
+    old = $.fn.validate;
+    $.fn.validate = function(config, callback) {
+      return new Validator(this).validate(config, callback);
+    };
+    return $.fn.validate.noConflict = function() {
+      $.fn.validate = old;
+      return this;
+    };
   };
 
 }).call(this);
